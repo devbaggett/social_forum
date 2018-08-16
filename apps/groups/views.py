7b93a4de3284cb2639c603f4c15264fb8d184b0a
devbaggett <devabaggett@gmail.com>
2018-08-15 17:58:55 -0700

    created 3 views and connected them to groups templates

--- apps/groups/views.py
+++ apps/groups/views.py
@@ -2,5 +2,24 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.core.urlresolvers import reverse
+from django.views import generic
 
-# Create your views here.
+from groups.models import Group, GroupMember
+
+
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+	# these are editable fields
+	fields = ('name', 'description')
+	# connect to Group model
+	model = Group
+
+class SingleGroup(generic.DetailView):
+	model = Group
+
+class ListGroups(generic.ListView):
+	model = Group
+
+
+
