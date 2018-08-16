ef57a09df6019efaccb48c9f7bd7954d2f0ced88
devbaggett <devabaggett@gmail.com>
2018-08-15 18:15:47 -0700

    connected groups views to urls and registered group with admin

--- apps/groups/admin.py
+++ apps/groups/admin.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class GroupMemberInLine(admin.TabularInline):
+	model = models.GroupMember
+
+
+admin.site.register(models.Group)
